3616fffcc8a653377d708aa304a419a9962f9852
Oprean Sergiu <oprean@gmail.com>
2015-03-13 22:45:15 +0200

    plaque edit popup work in progress

--- js/views/modals/EditPlaqueView.js
+++ js/views/modals/EditPlaqueView.js
@@ -2,15 +2,18 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'modules/Constants',
   'text!templates/plaque-edit.html'
-], function($, _, Backbone, editPlaqueTpl){
+], function($, _, Backbone, Constants, editPlaqueTpl){
 	var EditPlaqueView = Backbone.Modal.extend({
 		template: _.template(editPlaqueTpl),
 		submitEl: '.btn-submit',
+		cancelEl: '.btn-cancel',
 		events : {
 			
 		},
 		initialize : function() {
+			this.model.set({woodTypes : Constants.woodTypes });
 			console.log('init modal');
 		},
 		
