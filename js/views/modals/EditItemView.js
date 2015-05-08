db6395a300db9cd0fb02fec1e7a895bc0c83bd84
Oprean Sergiu <oprean@gmail.com>
2015-05-08 07:22:09 +0300

    file upload support, beginnings

--- js/views/modals/EditItemView.js
+++ js/views/modals/EditItemView.js
@@ -2,29 +2,48 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'modules/Utils',
   'modules/Constants',
   'text!templates/modals/item-edit.html',
+  'dropzone',
   'backbone.modal',
-], function($, _, Backbone, Constants, editItemTpl){
+], function($, _, Backbone, Utils, Constants, editItemTpl, dropzone){
 	var EditItemView = Backbone.Modal.extend({
 		template: _.template(editItemTpl),
 		submitEl: '.btn-submit',
 		cancelEl: '.btn-cancel',
 		events : {
-			
+			'click .image-url-tab' : 'toggleImageTabs',
+			'click .image-file-tab' : 'toggleImageTabs'
 		},
 		initialize : function() {
+			this.fileSource = 'url';
 			console.log('init modal');
+		},
+		
+		toggleImageTabs : function() {
+			this.fileSource = (this.fileSource == 'url')?'file':'url';
+			this.$('#image-url').toggle();
+			this.$('#image-file').toggle();
+			if (this.fileSource == 'url') {
+				this.$('.image-url-tab').parent().addClass('active');
+				this.$('.image-file-tab').parent().removeClass('active');				
+			} else {
+				this.$('.image-url-tab').parent().removeClass('active');
+				this.$('.image-file-tab').parent().addClass('active');
+			}
 		},
 		
 		templateHelpers : function() {
 			return {
+				fileSource : this.fileSource,
 				woodTypes : Constants.woodTypes,
 				shapeTypes : Constants.shapeTypes
 			};
 		},
 		
 		onRender : function() {
+			$("#image-file").dropzone({ url: "/file/post" });
 		},
 		
 		submit: function() {
@@ -38,6 +57,8 @@ define([
 				width : $('#width').val(),
 				height : $('#height').val(),
 			});
+			//this.$('#item-edit-form').submit();
+			Utils.upload('image-file',0);
 		}	
 			
 	});
